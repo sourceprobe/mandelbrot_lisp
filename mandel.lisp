f76c8ae59c43f8c04f9db82ccbbaf98ab95f9b53
SourceProbe team <git@sourceprobe.com>
2024-01-02 10:55:44 -0800

    Rewrite core to use LOOP instead of tail recursion.
    $ ./mandel.compile.sbcl && time ./mandel.sbcl.o
    
    Before:
    real    0m0.052s
    user    0m0.045s
    sys     0m0.008s
    
    After:
    real    0m0.034s
    user    0m0.034s
    sys     0m0.000s

--- mandel.lisp
+++ mandel.lisp
@@ -19,15 +19,13 @@
     (let (
       (xi (+ OUTPUT_OFFSET_X (* OUTPUT_RES_X (/ x RESOLUTION_X))))
       (yi (+ OUTPUT_OFFSET_Y (* OUTPUT_RES_Y (/ y RESOLUTION_Y)))))
-    (defun calc_inner (x y iters) 
-        (if (or (>= iters MAX_ITERATIONS)
-                (< 4 (+ (* x x) (* y y))))
-            iters
-            (let (
-                  (xnext (+ xi (* x x) (* -1 y y)))
-                  (ynext (+ yi (* x y 2))))
-                (calc_inner xnext ynext (+ 1 iters)))))
-    (calc_inner 0 0 0)))
+      (loop for iters below MAX_ITERATIONS
+	    for x = 0 then (+ (+ xi (* x x) (* -1 y y)))
+	    and y = 0 then (+ yi (* x y 2))
+	    when (< 4 (+ (* x x) (* y y)))
+	    	do (return iters)
+	    finally (return iters))))
+	
 (defun draw_window (width height) 
   (dotimes (y height)
     (dotimes (x width)
